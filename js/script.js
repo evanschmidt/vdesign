7dcebb726ecbfb964405ef5c6e6ce7406279ee64
schmook <eschmidt@vdesign.com>
2015-06-29 15:51:52 -0500

    removed hash

--- js/script.js
+++ js/script.js
@@ -25,48 +25,11 @@ Drupal.behaviors.my_custom_behavior = {
 $(function() {
   var $container = $('#isotope-container');
 
-  function getHashFilter() {
-    var hash = location.hash;
-    // get filter=filterName
-    var matches = location.hash.match( /filter=([^&]+)/i );
-    var hashFilter = matches && matches[1];
-    return hashFilter && decodeURIComponent( hashFilter );
-  }
   $container.isotope({
     itemSelector: '.isotope-element',
     filter: '.new'
   });
-  // bind filter button click
-  var $filters = $('#filters').on( 'click', 'button', function() {
-    var filterAttr = $( this ).attr('data-filter');
-    // set filter in hash
-    location.hash = 'filter=' + encodeURIComponent( filterAttr );
-  });
 
-  var isIsotopeInit = false;
-
-  function onHashchange() {
-    var hashFilter = getHashFilter();
-    if ( !hashFilter && isIsotopeInit ) {
-      return;
-    }
-    isIsotopeInit = true;
-    // filter isotope
-    $container.isotope({
-      itemSelector: '.element-item',
-      filter: hashFilter
-    });
-    // set selected class on button
-    if ( hashFilter ) {
-      $filters.find('.is-checked').removeClass('is-checked');
-      $filters.find('[data-filter="' + hashFilter + '"]').addClass('is-checked');
-    }
-  }
-
-  $(window).on( 'hashchange', onHashchange );
-  // trigger event handler to init Isotope
-  onHashchange();
-});
 });
 
 //Configure colorbox call back to resize with custom dimensions
